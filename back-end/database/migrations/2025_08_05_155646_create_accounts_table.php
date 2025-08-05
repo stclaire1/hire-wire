49d6cb6b9fa53e7f2e411d65cc472894effd8ccb
Ana Clara <anaccustodio8@gmail.com>
2025-08-05 15:58:59 -0300

    Create migrations, configured and tested database conection, installed laravel passport

---
+++ back-end/database/migrations/2025_08_05_155646_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->string('account_number')->unique();
+            $table->string('agency_number');
+            $table->string('account_type'); //checking, savings or investment
+            $table->decimal('balance', 10, 2)->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
