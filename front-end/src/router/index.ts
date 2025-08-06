141a6565cedee8b606a4bfa97660f844b48af66e
Ana Clara <anaccustodio8@gmail.com>
2025-08-06 16:44:54 -0300

    Create router file to handle navigation

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -1,8 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import { useAuthStore } from '@/stores/auth'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+      meta: { requiresGuest: true }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+      meta: { requiresGuest: true }
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+      meta: { requiresAuth: true }
+    }
+  ],
+})
+
+// navigation guards (route protection, executed before each route change)
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  authStore.initAuth()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else if (to.meta.requiresGuest && authStore.isAuthenticated) {
+    next('/home')
+  } else {
+    next()
+  }
 })
 
 export default router
